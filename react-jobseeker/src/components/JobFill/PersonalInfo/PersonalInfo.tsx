6477d4dc1a23f37987628352d0f9b294482e4c3a
mengzhuou <mou30@gatech.edu>
2024-08-04 18:39:48 -0400

    add personalInfo file

--- react-jobseeker/src/components/JobFill/PersonalInfo/PersonalInfo.tsx
+++ react-jobseeker/src/components/JobFill/PersonalInfo/PersonalInfo.tsx
@@ -1,23 +1,72 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { withFuncProps } from "../../withFuncProps";
+import "./PersonalInfo.css";
 
 interface PersonalInfoProps {
   navigate: ReturnType<typeof useNavigate>;
 }
 
-interface PersonalInfoState {}
+interface PersonalInfoState {
+  firstName: string;
+  lastName: string;
+}
 
 class PersonalInfo extends React.Component<PersonalInfoProps, PersonalInfoState> {
   constructor(props: PersonalInfoProps) {
     super(props);
-    this.state = {};
+    this.state = {
+      firstName: "",
+      lastName: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value } as Pick<PersonalInfoState, keyof PersonalInfoState>);
+  }
+
+  handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    alert("Saved!");
   }
 
   render() {
+    const { firstName, lastName } = this.state;
+
     return (
-      <div>
-        <h1>SWE Job Fill</h1>
+      <div className="container">
+        <h1>SWE Job Filler</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <label>
+              First Name:
+              <input
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={this.handleChange}
+                placeholder="Nina"
+              />
+            </label>
+            <label>
+              Last Name:
+              <input
+                type="text"
+                name="lastName"
+                value={lastName}
+                onChange={this.handleChange}
+                placeholder="Green"
+              />
+            </label>
+          </div>
+          <div className="row">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
